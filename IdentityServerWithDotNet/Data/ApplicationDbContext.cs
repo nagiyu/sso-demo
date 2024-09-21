fb7a058e0e3acbaccebfc07519dbff1936a7efc4
nagiyu <otonagiyu@gmail.com>
2024-09-21 17:39:46 +0900

    Add _ViewStart.cshtml to set the layout for views

---
+++ IdentityServerWithDotNet/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServerWithDotNet.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
