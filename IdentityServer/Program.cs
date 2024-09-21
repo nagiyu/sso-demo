75b40dff950a8c61107fc36eb138c4b3e609c770
nagiyu <otonagiyu@gmail.com>
2024-09-21 11:08:40 +0900

    Add ApiScopes and Clients to Config class

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,5 +1,16 @@
+using IdentityServer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddIdentityServer()
+    .AddDeveloperSigningCredential()        //This is for dev only scenarios when you don’t have a certificate to use.
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryClients(Config.Clients);
+
 var app = builder.Build();
+
+app.UseIdentityServer();
 
 app.MapGet("/", () => "Hello World!");
 
