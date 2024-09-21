425846f5826ddf68ff608a707c30ea212ca3ef65
nagiyu <otonagiyu@gmail.com>
2024-09-21 11:33:23 +0900

    Add IdentityController to handle GET request for /identity endpoint

---
+++ WebApplication1/Controllers/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -4,6 +4,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// Add authentication
+builder.Services.AddAuthentication("Bearer")
+    .AddIdentityServerAuthentication(options =>
+    {
+        options.Authority = "http://localhost:5000";
+        options.RequireHttpsMetadata = false;
+        options.ApiName = "api1";
+    });
+
+// Add MVC Core with Authorization and JSON formatters
+builder.Services.AddMvcCore()
+    .AddAuthorization()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null; // ここは必要に応じてカスタマイズできます
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -11,6 +28,9 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Use Authentication
+app.UseAuthentication();
 
 app.MapControllers();
 
